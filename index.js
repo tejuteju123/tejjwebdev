91bf07fc065b5eaf0f1fa5239299a5f47adf1b4f
shubham <188r1a04h7@gmail.com>
2020-12-26 16:38:39 +0530

    added index.js code

---
+++ index.js
@@ -0,0 +1,14 @@
+// 1.import or include like statement in express
+var express= require('express');
+//2.create application
+var app=express();
+//3.define function when somone access our site
+app.get('/',function(req,res){
+    res.send('hello sai gudmrg');
+})
+//4.run application on port
+var port=process.env.PORT ||3000;
+//once site is up function() will be called automatically
+app.listen(port,function(){
+    console.log("site is running on http://localhost:"+port);
+});
