22654fc36b2a5681ad98f0d1b53ff9c5eec3da87
shubham <188r1a04h7@gmail.com>
2020-12-27 14:06:07 +0530

    adding our own dummy cs and js file

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 var express= require('express');
 //2.create application
 var app=express();
+// Tell epress where css and js files are there
+app.use(express.static(__dirname+'/frontend'));
 //3.define function when somone access our site
 app.get('/',function(req,res){
     //res.send('hello bhanu this is my first website');
