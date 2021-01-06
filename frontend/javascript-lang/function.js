535cb60499079cf13ce80b4693d78ee9937a20f8
shubham <188r1a04h7@gmail.com>
2021-01-06 11:14:13 +0530

    functions in js

---
+++ frontend/javascript-lang/function.js
@@ -0,0 +1,27 @@
+//c or java
+//function add(int a, int b)
+//declaring function in js
+function add(a,b){
+return a+b;
+}
+//calling function
+var ans = add(25,25)
+console.log(ans);
+/*
+function name(input){
+
+}
+var a=function(input){
+    .....
+}
+var b =(inpt) => {
+
+}
+*/
+//function can be treated like varaiables
+// a function declare without name is called
+// Anonymous function
+// function(input){....}
+var prodfunction = function(a,b){
+    return a*b;
+}
