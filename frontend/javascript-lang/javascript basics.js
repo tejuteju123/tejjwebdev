a50c58bbe9a41565cee58a6238d9e6a8d46a2769
shubham <188r1a04h7@gmail.com>
2021-01-06 10:39:03 +0530

    js-basics

---
+++ frontend/javascript-lang/javascript basics.js
@@ -0,0 +1,41 @@
+//1.console.log & process.stdout.write
+// 2. varaiable CSSStyleDeclaration
+// 3.datatype [typeof(x)]-->Number,boolean,String
+// 4.operations
+// 5.conditional statmnets
+// 6.loops
+
+
+
+
+//'use strict'
+ //x=5;
+ //commenting out lines in shortcut
+ //ctrl   +  '/'   ----> IN WINDOWS
+//console.log(x);
+//var x=5;
+// console.log(x);
+// console.log('hello');
+// console.log("hello");
+//Without newline in the end
+// process.stdout.write("hello");
+// process.stdout.write("world");
+// process.stdout.write(x+'');
+// var x='this is teju';
+// var y=12;//12.34 number can be floating values also
+// var z=true;
+// const d = 19;
+ var b = 2**5;
+ console.log(b);
+// console.log(x);
+// console.log(y);
+// console.log(z);
+// console.log(typeof(x) + " :" +x);
+// console.log(typeof(y)  + " :" + y);
+// console.log(typeof(z) + " :" + z);
+// console.log(typeof(d) + " :" + d);
+var x=2;
+if(x==2)// number get converted in to the string
+console.log('this works');
+if(x===2)
+console.log("this doesnt work")
